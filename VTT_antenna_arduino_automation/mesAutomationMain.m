d6b75ad669bf109c0e4152286bde4f4974b3c2c3
egeb1 <168190162+egeb1@users.noreply.github.com>
2024-07-11 09:58:25 +0300

    Add files via upload

--- VTT_antenna_arduino_automation/mesAutomationMain.m
+++ VTT_antenna_arduino_automation/mesAutomationMain.m
@@ -1,23 +1,30 @@
+%% SETUP
+
 % Main code for controlling arduino and VNA
 % Eero Pietiläinen 10.8.2023
 % modified 14.6.2024 by Eero Pietiläinen
+
 clc
 addpath('PNARead\')
 addpath('VTT_antenna_arduino_automation\')
 addpath('Standa\Bergman')
 
-% for-loop to go over all 64 elements. 
-for activeElement = 1:1:64
+% Loop that runs the moveToHome so many times that all devices are in zero
+% position.
+keepRunning = true;
+while keepRunning
+    moveToHome();
+    userResponse = input('Do you want to run the function again? (y/n): ', 's');
+    if strcmpi(userResponse, 'n')
+        keepRunning = false;
+    end
+end
 
-disp(['measuring element:',num2str(activeElement)] )
-try
 % set VNA values from VNA itself. Example set frequency and mmwSetup. Set f
 % range in matlab code to same freq range and as in VNA. NumPoints as well
 % check from VNA.
 % set how many rows of data inputed to arduino
 numOfControlUnits = 64;
-% Element set to be active -6dB?
-%activeElement = 1;
 % frequency
 f = [70e9, 80e9];
 % number of measurement points
@@ -25,21 +32,19 @@ numPoints = 21;
 % arduino COM port
 comPort ='COM5';
 
+%% Main Loop for measurement 
+
+
+% for-loop to go over all 64 elements. 
+for activeElement = 1:1:64
+
+disp(['measuring element:',num2str(activeElement)] )
+try
 
 % Script to run the functions in parallel
 % Create a parallel pool if it does not exist
 if isempty(gcp('nocreate'))
     parpool;
-end
-
-% Determine how much move mask and probe when elements are
-% measured from 1 to 64. Y position should always move and X pos moves
-% after 8 elements have been measured
-xpos = floor((activeElement - 1) / 8) * dx;
-ypos = mod((activeElement - 1), 8) * dy;
-
-if(mod(activeElement,4)==0)
-    moveToHome
 end
 
 % Run updating DACs and moving mask&probe in parallel to save time
@@ -50,19 +55,12 @@ f2 = parfeval(@moveMaskAndProbe, 0,activeElement);
 wait(f1)
 wait(f2)
 
-% update arduino file with elements DAC values.
-%updateDACs(activeElement,comPort)
-
-% pause; to stop code from running
-% until enter is pressed so there is time to move measurement probe and
-% mask.
-pause;
 measurementName = 'element wise measurements';
 % folder where to save measurement data
 folderPathAndName = ['C:\Users\meas\Desktop\2024_mittaukset\augustMeas\' [num2str(activeElement),'_D0_16_32_255_D1_16_32_255']];
 makeFolder = mkdir(folderPathAndName);
-% Connect to arduino
 
+% Connect to arduino
 try
     serialConnection = serialport(comPort, 230400);
     disp('Serial monitor opened successfully.');
@@ -76,7 +74,6 @@ end
 pause(4);
 % Loop to go trough diffrent control units and measure data.
 for i = 1 : numOfControlUnits
-    
     setControlUnit(serialConnection,i-1); %i-1 because control units start from 0 in arduino
     read(serialConnection,serialConnection.NumBytesAvailable,'string')
     pause(3);
@@ -85,10 +82,9 @@ for i = 1 : numOfControlUnits
     
 end
 read(serialConnection,serialConnection.NumBytesAvailable,'string')
+
 % close connection to arduino
 delete(serialConnection);
-
-
 
 
 catch ME
